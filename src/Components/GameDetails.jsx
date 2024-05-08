ccb2ec4f085ef33b2246dfebd02cac73f26ede65
johnnyji6 <johnnyji4091@gmail.com>
2024-05-08 22:31:54 +0200

    Some progress on the GameDetails.jsx design

--- src/Components/GameDetails.jsx
+++ src/Components/GameDetails.jsx
@@ -140,9 +140,9 @@ export default function GameDetails() {
         </div>
       </div>
 
-      <div className="container mt-4 row mx-auto">
+      <div className="container my-4 row mx-auto">
         <div className="col-8">
-          <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+          <Tabs defaultActiveKey="offers" id="uncontrolled-tab-example" className="mb-3">
             <Tab eventKey="offers" title="Offers" className="text-white">
               <div className="d-flex flex-column align-items-center mt-2">
                 <div className="d-flex align-items-center justify-content-center rounded bg-secondary" style={{ width: "30%" }}>
@@ -157,12 +157,12 @@ export default function GameDetails() {
                 </div>
               </div>
               <div>
-                <h3 className="w-25 mt-5 border-bottom pb-2">Offers</h3>
+                <h3 className="w-25 mt-5 border-bottom border-3 pb-2">Offers</h3>
                 <div className="row">
                   {gameData &&
                     gameData.deals.map((deal) => (
                       <div
-                        className="card gameDetails__card bg-black text-light rounded-0 mx-auto p-0 d-flex justify-content-center mb-1"
+                        className="card gameDetails__card--offers bg-black text-light rounded-0 mx-auto p-0 d-flex justify-content-center mb-1"
                         key={deal.dealID}>
                         <div className="row g-0 align-items-center">
                           <div className="col-2">
@@ -170,23 +170,36 @@ export default function GameDetails() {
                               <img src={`/img/stores/${findStoreById(deal.storeID).banner}`} alt="Store Banner" className="img-fluid" />
                             </div>
                           </div>
-                          <div className="col-9">
-                            <div className="row justify-content-between align-items-center">
-                              <div className="col-xl-9 col-lg-8 col-md-9 col-sm-9 col-8">
+                          <div className="col-10">
+                            <div className="row justify-content-between align-items-center me-4">
+                              <div className="col-xl-6 col-lg-5 col-md-9 col-sm-9 col-8">
                                 <p className="card-text text-truncate m-0 ms-2" title={gameData.info.title}>
                                   {gameData.info.title}
                                 </p>
                               </div>
-                              <div className="col-xl-3 col-lg-4 col-md-3 col-sm-3 col-4">
+                              <div className="col-lg-3 col-md-3 col-sm-3 col-4">
                                 <p className="m-0 text-end">
                                   {deal.retailPrice === deal.price ? (
                                     <span className="fw-bold fs-5">{deal.price}€</span>
                                   ) : (
                                     <>
                                       <span className="text-decoration-line-through text-secondary">{deal.retailPrice}€</span>
-                                      <span className="fw-bold fs-5">{deal.price}€</span>
+                                      <span className="fw-bold fs-5">{deal.price}€</span> <br />
+                                      <span className="fs-6 text-secondary fst-italic">-{Math.round(deal.savings)}%</span>
                                     </>
                                   )}
+                                </p>
+                              </div>
+                              <div className="col-xl-3">
+                                <p className="m-0 text-end">
+                                  <a
+                                    href={`https://www.cheapshark.com/redirect?dealID=${deal.dealID}`}
+                                    target="_blank"
+                                    className="btn btn-primary"
+                                    rel="noreferrer">
+                                    Take a look!
+                                    <i class="fa-solid fa-up-right-from-square"></i>
+                                  </a>
                                 </p>
                               </div>
                             </div>
@@ -206,19 +219,43 @@ export default function GameDetails() {
           </Tabs>
         </div>
         <div className="col-4">
-          <div className="card border-0 bg-dark text-white">
+          <div className="card border-0 rounded-0 bg-black text-white sticky-top top-25 gameDetails__card">
             {console.log(gameData)}
             <img
               className="card-img-top img-fluid rounded-0"
               src={moreGameData ? moreGameData.background_image_additional : ""}
               alt="Title"
             />
-            <div className="card-body p-0 pt-2">
-              <p className="card-text">Release Date: {moreGameData ? moreGameData.released : ""}</p>
-              <p className="card-text">Developers: {moreGameData ? moreGameData.developers[0].name : ""}</p>
-              <p className="card-text">Genres: {moreGameData ? moreGameData.genres.map((genre) => genre.name).join(", ") : ""}</p>
-              <p className="card-text">
-                Platforms: {moreGameData ? moreGameData.parent_platforms.map((platform) => platform.platform.name).join(", ") : ""}
+            <div className="card-body px-4 pt-4 pb-0">
+              <p className="card-text row">
+                <div className="col-4">
+                  <span className="text-secondary">Release Date:</span>
+                </div>
+                <div className="col-6">
+                  <span>{moreGameData ? moreGameData.released : ""}</span>
+                </div>
+              </p>
+              <p className="card-text row">
+                <div className="col-4">
+                  <span className="text-secondary">Developers:</span>
+                </div>
+                <div className="col-6">
+                  <span>{moreGameData ? moreGameData.developers[0].name : ""}</span>
+                </div>
+              </p>
+              <p className="card-text row">
+                <div className="col-4">
+                  <span className="text-secondary">Platforms:</span>
+                </div>
+                <div className="col-6">
+                  <span>{moreGameData ? moreGameData.parent_platforms.map((platform) => platform.platform.name).join(", ") : ""}</span>
+                </div>
+              </p>
+
+              <p className="card-text d-flex flex-wrap">
+                {moreGameData
+                  ? moreGameData.genres.map((genre) => <div className="btn btn-dark text-white mx-2 mb-3">{genre.name}</div>)
+                  : ""}
               </p>
             </div>
           </div>
